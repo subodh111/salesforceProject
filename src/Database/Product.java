ddaee88b3b82d5647326fd837661b12c57860ec2
CurtisJamesPhan <curtisjamesphan@gmail.com>
2019-04-19 15:29:53 -0700

    stage gradle

---
+++ src/Database/Product.java
@@ -0,0 +1,27 @@
+class Product {
+    private String name = "blank";
+    private double price = -1.0;
+    
+    public Product(String name,double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+}
